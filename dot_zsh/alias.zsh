e5d81fded2c3160ee8312db6883c22896cb34aea
TED Vortex <ted.vortex@gmail.com>
2022-06-03 02:50:49 +0200

    fix: replace some commands with improved binaries

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -1,9 +1,17 @@
+# command replacements
+alias cat='bat'
+alias du='duf'
+alias find='fd'
+
+# command shortcuts
 alias ls='ls --color'
 alias la='ls -a'
 alias ll='ls -lahH'
 alias lal='ls -al'
+
 alias d='dirs -v'
 alias dus='du -s'
 alias el='exa --tree --level=2 --long'
 alias ell='exa --header --long --git --group --created --modified --inode --grid'
 alias npmgc='npm ls -gp --depth=0 | awk -F/ "/node_modules/ && !/\/npm$/ {print $NF}" | xargs npm -g rm'
+
