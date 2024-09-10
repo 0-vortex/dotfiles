dc23618f4ee889c6f63ad50a02315940e9d40651
TED Vortex <ted.vortex@gmail.com>
2024-09-11 00:07:09 +0200

    fix: fully correct debian installation and update packages

--- dot_zsh/env.zsh
+++ dot_zsh/env.zsh
@@ -9,36 +9,65 @@ export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
 
 # Spaceship settings
 export SPACESHIP_PROMPT_ORDER=(
-  time          # Time stamps section
-  user          # Username section
-  dir           # Current directory section
-  host          # Hostname section
-  git           # Git section (git_branch + git_status)
-  package       # Package version
-  node          # Node.js section
-  ruby          # Ruby section
-  xcode         # Xcode section
-  swift         # Swift section
-  golang        # Go section
-  rust          # Rust section
-  julia         # Julia section
-  docker        # Docker section
-  aws           # Amazon Web Services section
-  conda         # conda virtualenv section
-  python        # Python section
-  kubectl       # Kubectl context section
-  terraform     # Terraform workspace section
-  exec_time     # Execution time
-  line_sep      # Line break
-  battery       # Battery level and status
-  jobs          # Background jobs indicator
-  exit_code     # Exit code section
-  char          # Prompt character
+  time           # Time stamps section
+    user           # Username section
+    dir            # Current directory section
+    host           # Hostname section
+    git            # Git section (git_branch + git_status)
+    package        # Package version
+    node           # Node.js section
+    bun            # Bun section
+    deno           # Deno section
+    ruby           # Ruby section
+    python         # Python section
+    xcode          # Xcode section
+    swift          # Swift section
+    golang         # Go section
+    perl           # Perl section
+    php            # PHP section
+    rust           # Rust section
+    kotlin         # Kotlin section
+    java           # Java section
+    julia          # Julia section
+    docker         # Docker section
+    docker_compose # Docker section
+    aws            # Amazon Web Services section
+    gcloud         # Google Cloud Platform section
+    azure          # Azure section
+    conda          # conda virtualenv section
+    vlang          # V section
+    zig            # Zig section
+    kubectl        # Kubectl context section
+    terraform      # Terraform workspace section
+    pulumi         # Pulumi stack section
+    nix_shell      # Nix shell
+    exec_time      # Execution time
+    async          # Async jobs indicator
+    line_sep       # Line break
+    battery        # Battery level and status
+    jobs           # Background jobs indicator
+    exit_code      # Exit code section
+    sudo           # Sudo indicator
+    char           # Prompt character
 )
+
+export SPACESHIP_ANSIBLE_SHOW=false
+export SPACESHIP_CRYSTAL_SHOW=false
+export SPACESHIP_DART_SHOW=false
+export SPACESHIP_DOTNET_SHOW=false
+export SPACESHIP_ELIXIR_SHOW=false
+export SPACESHIP_ELM_SHOW=false
+export SPACESHIP_ERLANG_SHOW=false
+export SPACESHIP_GNU_SCREEN_SHOW=false
+export SPACESHIP_HASKELL_SHOW=false
+export SPACESHIP_JULIA_SHOW=false
+export SPACESHIP_HG_SHOW=false
+export SPACESHIP_SCALA_SHOW=false
+
 export SPACESHIP_TIME_SHOW=true
 export SPACESHIP_CONDA_SHOW=true
 export SPACESHIP_BATTERY_THRESHOLD=50
-export SPACESHIP_DOCKER_CONTEXT_SHOW=false
+export SPACESHIP_DOCKER_CONTEXT_SHOW=true
 export SPACESHIP_EXIT_CODE_SHOW=true
 
 export GEM_HOME="${HOME}/.gem"
