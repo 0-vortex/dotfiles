d2a16486160a8c35d803f4d48d45d3ade8b0157b
TED Vortex <ted.vortex@gmail.com>
2024-09-18 20:22:36 +0200

    build: minor package and alias improvements

--- dot_zsh/alias.zsh
+++ dot_zsh/alias.zsh
@@ -1,19 +1,17 @@
-# command replacements
-alias cat='bat -A --terminal-width -1'
-alias du='duf'
-alias find='fd'
-
 # command shortcuts
 alias ls='ls --color'
 alias la='ls -a'
 alias ll='ls -lahH'
-alias lal='ls -al'
+alias npmgc='npm ls -gp --depth=0 | awk -F/ "/node_modules/ && !/\/npm$/ {print $NF}" | xargs npm -g rm'
+alias md5sum='md5 -r'
 
-alias d='dirs -v'
+# go aliases
+alias du='duf'
 alias dus='du -s'
+
+# rust aliases
+alias cat='bat -A --terminal-width -1'
 alias el='eza --tree --level=2 --long'
 alias ell='eza --header --long --git --group --created --modified --inode --grid'
-alias npmgc='npm ls -gp --depth=0 | awk -F/ "/node_modules/ && !/\/npm$/ {print $NF}" | xargs npm -g rm'
-
-# 3rd party requirements
-alias md5sum='md5 -r'
+alias find='fd'
+alias hex='hexyl --border=none'
